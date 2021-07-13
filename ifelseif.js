005793e0ecbe0adf33ee2aebed44cc95fdc8c1e2
HanNPC <86652190+HanNPC@users.noreply.github.com>
2021-07-13 20:54:56 +0700

    Add files via upload

---
+++ ifelseif.js
@@ -0,0 +1,12 @@
+        let a=20
+        let b=25
+        let c=30
+        let d=40
+
+        if (b > a) {
+            console.log('Terbesar : ' + b)
+        } else if (c > b) {
+            console.log('Terbesar : ' + c)
+        } else if (d > c) {
+            console.log('Terbesar : ' + d)
+        }
